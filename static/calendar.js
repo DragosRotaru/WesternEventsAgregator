91fc4be0bc615f81bb968065969e6d1c285cd4cd
Dragos Rotaru <info@dragosrotaru.com>
2015-12-18 16:41:35 -0500

    adding in fullcalendar

--- static/calendar.js
+++ static/calendar.js
@@ -1,10 +1,10 @@
-$(function () {
+$j(function () {
 	var date = new Date();
 	var d = date.getDate();
 	var m = date.getMonth();
 	var y = date.getFullYear();
 
-	$('#calendar').fullCalendar({
+	$j('#calendar').fullCalendar({
 		header: {
 			left: 'prev,next today',
 			center: 'title',
--- static/tabmenu.js
+++ static/tabmenu.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
+$i(document).ready(function() {
 
-  $('.tab-content>div:not(":first-of-type")').hide();
-  $('<div class="line"></div>').appendTo('.tab-menu li');
-  $('.tab-menu li:first-of-type').find(':first').width('100%')
+  $i('.tab-content>div:not(":first-of-type")').hide();
+  $i('<div class="line"></div>').appendTo('.tab-menu li');
+  $i('.tab-menu li:first-of-type').find(':first').width('100%')
 
-  $('.tab-menu li').each(function(i) {
-    $(this).attr('data-tab', 'tab'+i);
+  $i('.tab-menu li').each(function(i) {
+    $i(this).attr('data-tab', 'tab'+i);
   });
 
-  $('.tab-content>div').each(function(i) {
-    $(this).attr('data-tab', 'tab'+i);
+  $i('.tab-content>div').each(function(i) {
+    $i(this).attr('data-tab', 'tab'+i);
   });
 
-  $('.tab-menu li').on('click', function() {
+  $i('.tab-menu li').on('click', function() {
 
-    var dataTab = $(this).data('tab');
-    var getWrapper = $(this).closest('.tab-wrapper');
-    var line = $(this).find('.line');
+    var dataTab = $i(this).data('tab');
+    var getWrapper = $i(this).closest('.tab-wrapper');
+    var line = $i(this).find('.line');
 
     getWrapper.find('.tab-menu li').removeClass('active');
-    $(this).addClass('active');
+    $i(this).addClass('active');
 
     getWrapper.find('.line').width(0);
     line.animate({'width':'100%'}, 'fast');
