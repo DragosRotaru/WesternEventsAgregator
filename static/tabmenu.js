4aaff9451e0d80eb5da9267e8260cf63e5e53478
Dragos Rotaru <info@dragosrotaru.com>
2015-12-19 12:19:54 -0500

    resolving jquery instances to single version

--- static/tabmenu.js
+++ static/tabmenu.js
@@ -1,25 +1,25 @@
-$i(document).ready(function() {
+$(document).ready(function() {
 
-  $i('.tab-content>div:not(":first-of-type")').hide();
-  $i('<div class="line"></div>').appendTo('.tab-menu li');
-  $i('.tab-menu li:first-of-type').find(':first').width('100%')
+  $('.tab-content>div:not(":first-of-type")').hide();
+  $('<div class="line"></div>').appendTo('.tab-menu li');
+  $('.tab-menu li:first-of-type').find(':first').width('100%')
 
-  $i('.tab-menu li').each(function(i) {
-    $i(this).attr('data-tab', 'tab'+i);
+  $('.tab-menu li').each(function(i) {
+    $(this).attr('data-tab', 'tab'+i);
   });
 
-  $i('.tab-content>div').each(function(i) {
-    $i(this).attr('data-tab', 'tab'+i);
+  $('.tab-content>div').each(function(i) {
+    $(this).attr('data-tab', 'tab'+i);
   });
 
-  $i('.tab-menu li').on('click', function() {
+  $('.tab-menu li').on('click', function() {
 
-    var dataTab = $i(this).data('tab');
-    var getWrapper = $i(this).closest('.tab-wrapper');
-    var line = $i(this).find('.line');
+    var dataTab = $(this).data('tab');
+    var getWrapper = $(this).closest('.tab-wrapper');
+    var line = $(this).find('.line');
 
     getWrapper.find('.tab-menu li').removeClass('active');
-    $i(this).addClass('active');
+    $(this).addClass('active');
 
     getWrapper.find('.line').width(0);
     line.animate({'width':'100%'}, 'fast');
