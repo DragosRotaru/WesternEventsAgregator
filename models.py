fd6b5566736d9542eb630b7c9f37ad060ddc0179
Dragos Rotaru <info@dragosrotaru.com>
2016-03-14 14:10:01 -0400

    changes

--- models.py
+++ models.py
@@ -1,11 +1,74 @@
 from app import db
 
-class S_Link(db.Model):
+#######CATEGORY#######
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Category %r>' % (self.nickname)
+
+#######EMAIL#######
+
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), index=True unique=True)
+    entity = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Email %r>' % (self.email)
+
+#######SUGGESTIONS#######
+
+#TODO: ADD FIELDS
+class S_Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    url = db.Column(db.String(128), index=True, unique=True)
+    category = db.Column(db.Integer, db.ForeignKey('Category.id'))
+    description = db.Column(db.String(128), unique=True)
+
+    def __repr__(self):
+        return '<S_Event %r>' % (self.nickname)
+
+#TODO: ADD FIELDS
+class S_Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    url = db.Column(db.String(128), index=True, unique=True)
+    category = db.Column(db.Integer, db.ForeignKey('Category.id'))
+    description = db.Column(db.String(128), unique=True)
+
+    def __repr__(self):
+        return '<S_Group %r>' % (self.nickname)
+
+
+#######ENTITIES#######
+
+#TODO: ADD properties
+class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     url = db.Column(db.String(128), index=True, unique=True)
     category = db.Column(db.String(32), index=True)
     description = db.Column(db.String(128), unique=True)
+    timestamp = db.Column(db.DateTime)
+    dateadded = db.Column(db.DateTime)
+    Category = db.Column(db.Integer, db.ForeignKey('Category.id'))
+	email = db.Column(db.Integer, db.ForeignKey('Email.id'))
 
     def __repr__(self):
-        return '<S_Link %r>' % (self.nickname)
+        return '<Event %r>' % (self.nickname)
+
+#TODO: ADD properties
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    url = db.Column(db.String(128), index=True, unique=True)
+    description = db.Column(db.String(128), unique=True)
+    dateadded = db.Column(db.DateTime)
+    category = db.Column(db.Integer, db.ForeignKey('Category.id'))
+
+    def __repr__(self):
+        return '<Group %r>' % (self.nickname)
--- views.py
+++ views.py
@@ -20,8 +20,8 @@ def showemailpage():
     # return "Display newsletter signup"
     return render_template('email.html')
 
-@app.route('/shareevent', methods=['GET', 'POST'])
-@app.route('/shareevent.html', methods=['GET', 'POST'])
+@app.route('/share', methods=['GET', 'POST'])
+@app.route('/share.html', methods=['GET', 'POST'])
 def showshareeventpage():
     # return "Display ShareEventForm"
     form = ShareEventForm()
@@ -29,23 +29,3 @@ def showshareeventpage():
         flash('Event Shared: %s' % (form.title.data))
         return redirect('/')
     return render_template('shareevent.html', form=form)
-
-@app.route('/sharelink', methods=['GET', 'POST'])
-@app.route('/sharelink.html', methods=['GET', 'POST'])
-def showsharelinkpage():
-    # return "Display ShareLinkForm"
-    form = ShareLinkForm()
-    if form.validate_on_submit():
-        flash('Link Received: %s' % (form.url.data))
-        return redirect('/')
-    return render_template('sharelink.html', form=form)
-
-@app.route('/sharesource', methods=['GET', 'POST'])
-@app.route('/sharesource.html', methods=['GET', 'POST'])
-def showsharesourcepage():
-    # return "Display ShareSourceForm"
-    form = ShareSourceForm()
-    if form.validate_on_submit():
-        flash('URL Reveived: %s' % (form.url.data))
-        return redirect('/')
-    return render_template('sharesource.html', form=form)
